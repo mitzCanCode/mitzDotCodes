78f023c54df5b48eba4989d4ad0750103517fe0d
Dimitris Chatzigeorgiou <dimitris.chatzi@proton.me>
2026-07-07 02:56:24 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,47 @@
+import './style.css'
+
+
+document.querySelector('#screen').innerHTML = `
+
+<div class="crt-display">
+
+
+  <div class="crt-scanlines"></div>
+  <div class="crt-noise"></div>
+  <div class="crt-flicker"></div>
+
+
+
+  <div class="
+    crt-content
+    text-white
+    font-mono
+    text-center
+  ">
+
+
+    <p class="text-green-400">
+      SYSTEM ONLINE
+    </p>
+
+
+    <h1 class="
+      text-6xl
+      tracking-widest
+      mt-5
+    ">
+      MITZZZ.DEV
+    </h1>
+
+
+    <p>
+      SOFTWARE ENGINEER
+    </p>
+
+
+  </div>
+
+
+</div>
+
+`
