6a0bfb8f9e8464d58fada8684195c4f408bbc01d
Dimitris Chatzigeorgiou <dimitris.chatzi@proton.me>
2026-07-07 20:21:41 +0300

    Created launch screen

--- src/main.js
+++ src/main.js
@@ -1,47 +1,116 @@
 import './style.css'
 
+const screen = document.querySelector('#screenContent');
 
-document.querySelector('#screen').innerHTML = `
+const lines = [
+  { text: "SYSTEM ONLINE...", type: "system" },
+  { text: "", type: "" },
 
-<div class="crt-display">
+  { text: "IDENTITY:", type: "label" },
+  { text: "MITZ", type: "value" },
 
+  { text: "", type: "" },
 
-  <div class="crt-scanlines"></div>
-  <div class="crt-noise"></div>
-  <div class="crt-flicker"></div>
+  { text: "ROLE:", type: "label" },
+  { text: "SOFTWARE ENGINEER", type: "value" },
 
+  { text: "", type: "" },
 
+  { text: "MISSION:", type: "label" },
+  { text: "Building scalable web experiences", type: "mission" },
+  { text: "and digital products that feel alive.", type: "mission" },
 
-  <div class="
-    crt-content
-    text-white
-    font-mono
-    text-center
-  ">
-
-
-    <p class="text-green-400">
-      SYSTEM ONLINE
-    </p>
-
-
-    <h1 class="
-      text-6xl
-      tracking-widest
-      mt-5
-    ">
-      MITZZZ.DEV
-    </h1>
+  { text: "", type: "" },
+];
 
 
-    <p>
-      SOFTWARE ENGINEER
-    </p>
+let index = 0;
 
 
-  </div>
+screen.innerHTML = `
+<div class="terminal">
 
+  <div id="typedText"></div>
+
+  <button id="enterSystem" class="enter-system hidden">
+    <span class="cursor">[ENTER SYSTEM]</span>
+  </button>
 
 </div>
+`;
 
-`
+
+
+const typedText = document.querySelector("#typedText");
+const enterSystem = document.querySelector("#enterSystem");
+
+enterSystem.addEventListener("click", () => {
+  console.log("ENTER SYSTEM");
+});
+
+
+
+function typeLine() {
+
+  if(index >= lines.length){
+
+    setTimeout(() => {
+      enterSystem.classList.remove("hidden");
+    },500);
+
+    return;
+  }
+
+
+  const current = lines[index];
+
+
+  const line = document.createElement("div");
+
+  line.className = current.type;
+  if (current.type === "system") {
+    line.classList.add("text-center");
+  }
+
+  typedText.appendChild(line);
+
+
+  // empty line
+  if(current.text === "") {
+
+    index++;
+
+    setTimeout(typeLine,180);
+
+    return;
+
+  }
+
+
+  let char = 0;
+
+
+  const interval = setInterval(()=>{
+
+    line.textContent += current.text[char];
+
+    char++;
+
+
+    if(char >= current.text.length){
+
+      clearInterval(interval);
+
+      index++;
+
+      setTimeout(typeLine,180);
+
+    }
+
+
+  },45);
+
+}
+
+
+setTimeout(typeLine,1500);
